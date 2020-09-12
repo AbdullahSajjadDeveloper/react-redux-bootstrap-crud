22d5aa25062606109ce49e1b4da0c224cfc03957
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-09-12 11:00:13 +0500

    Updates

---
+++ precache-manifest.3e0628691e4c370bfab185920eda9211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf589d273922c97e6c60b22b22030af",
+    "url": "/react-redux-bootstrap-crud/index.html"
+  },
+  {
+    "revision": "347f04c7b328c4157a0d",
+    "url": "/react-redux-bootstrap-crud/static/css/main.1c97001b.chunk.css"
+  },
+  {
+    "revision": "8517292496777e866327",
+    "url": "/react-redux-bootstrap-crud/static/js/2.2c97b3f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-bootstrap-crud/static/js/2.2c97b3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347f04c7b328c4157a0d",
+    "url": "/react-redux-bootstrap-crud/static/js/main.9ade1b85.chunk.js"
+  },
+  {
+    "revision": "8136cea8e25583e291d5",
+    "url": "/react-redux-bootstrap-crud/static/js/runtime-main.f82912cf.js"
+  }
+]);
